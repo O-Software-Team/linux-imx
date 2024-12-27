d666b3f5240c7c6a8b2e8171b3bb97f55bacfc06
Dan Fields <fieldsda@gmail.com>
2024-12-26 21:11:28 -0500

    Revert "MGS-7326 [#imx-2976] 0083-CL709461-KERNEL-SPACE-22Q2_NXP-ocl30-qnx-Merging-671"
    This reverts commit de8ebfd7ba292f1af20441a546d7614ebf1e5d3f.

--- drivers/mxc/gpu-viv/hal/kernel/inc/gc_hal_base.h
+++ drivers/mxc/gpu-viv/hal/kernel/inc/gc_hal_base.h
@@ -284,7 +284,7 @@ typedef struct _gcsSystemInfo {
     gcvFALSE, /* exiting            */               \
     gcvFALSE, /* Special flag for NP2 texture. */    \
     gcvFALSE, /* device open.       */               \
-    {gcvNULL}, /* destructor         */               \
+    gcvNULL, /* destructor         */               \
     gcvNULL, /* accessLock         */               \
     gcvNULL, /* GL FE compiler lock*/               \
     gcvNULL, /* CL FE compiler lock*/               \
@@ -920,7 +920,6 @@ gcoHAL_CommandBufferAutoCommit(gcoHAL Hal, gctBOOL AutoCommit);
 
 gceSTATUS
 gcoHAL_CommandBufferAutoSync(gcoHAL Hal, gctBOOL AutoSync);
-
 
 #if gcdENABLE_MULTI_DEVICE_MANAGEMENT
 gceSTATUS
@@ -2378,7 +2377,6 @@ gcoHEAP_ProfileStart(IN gcoHEAP Heap);
 gceSTATUS
 gcoHEAP_ProfileEnd(IN gcoHEAP Heap, IN gctCONST_STRING Title);
 #endif
-
 
 /******************************************************************************
  ******************************* Debugging Macros *****************************
